6fad8879c2de9910b9e50d25db161ca804dd7e13
Max Cardwell <maximillian.cardwell@gmail.com>
2022-02-21 17:02:18 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+#create some variables
+
+number1 = 15
+number2 = 25
+number3 = 202
+
+sum = 0
+
+#make a function
+
+def total_sum(first, second):
+    return first + second 
+    
+#call on function
+
+print(total_sum(number1,number3))
